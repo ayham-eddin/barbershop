128af0efa7440dd8c454d58317695e32fb9384f2
Ayham Alaa Eddin <ayham.alaaeddin@gmail.com>
2025-10-26 22:22:23 +0100

    add backend and mongoDB setup

---
+++ backend/src/common/constants/ENV.ts
@@ -0,0 +1,22 @@
+import jetEnv, { num, str } from 'jet-env';
+import { isEnumVal } from 'jet-validators';
+
+import { NodeEnvs } from '.';
+
+
+/******************************************************************************
+                                 Setup
+******************************************************************************/
+
+const ENV = jetEnv({
+  NodeEnv: isEnumVal(NodeEnvs),
+  Port: num,
+  MongoUri: str,
+});
+
+
+/******************************************************************************
+                            Export default
+******************************************************************************/
+
+export default ENV;
